d5773f47d25f9361cc4ecae46bad0fa87f2cbcc0
Andersen <97132038+AndersenNgai@users.noreply.github.com>
2024-07-31 16:35:33 +0000

    Added the madlibs.js file

--- script.js
+++ script.js
@@ -5,23 +5,41 @@ let nemo = 10;
 console.log(9+5);
 console.log(9 + 9 * nemo);
 // Create a variable to hold your name as a string
+let name = "Andersen";
+name = "Rick";
+console.log(name);
 //Concatenate your name variable into a message and log it to the console.
+const someText = ", good morning";
 //Example: “Hello, Zach!” 
-
+console.log(name + someText);
 //console log your name variable
 
 //Re-assign the value of your name variable below its declaration and console log it again
 
 //practice math operators
-
+console.log();
+console.log(99.3 + 8.22);
+console.log(22.3 - 998325);
+console.log(5 * 3.5);
+console.log(2 / 8);
+console.log(9 % 4);
+console.log(6 ** 3);
 // boolean - true or false
-
+let somebool = true;
+console.log(somebool);
+console.log(!somebool);
 //create null and undefined
-
+let undThing;
+console.log(undThing);
+let nullThing = null;
+console.log(nullThing);
 //mix data types
-
+console.log("Hi" + 35);
 //string methods
-
+console.log(typeof 999.3);
+let aStringNumber = "99.3";
+let aNumberNumber = Number("99.3");
+console.log(aNumberNumber, typeof aNumberNumber);
 //link up html file and create prompt and save value
 
 //access the DOM
